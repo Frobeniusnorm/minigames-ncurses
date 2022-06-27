9b7ad1a12f6a3f908c5e6f3d58b7125767346ec1
David Schwarzbeck <xe87keme@stud.informatik.uni-erlangen.de>
2022-06-27 15:46:38 +0200

    created save file interface

--- safefiles/save.c
+++ safefiles/save.c
@@ -43,6 +43,14 @@ static void findPath(char **dir, char **file) {
     *file = fpath;
   }
 }
+SaveFile* loadSaveFile();
+void updateSaveFile();
+void freeSaveFile(SaveFile*);
+
+GameData* findGame(SaveFile* file, char* gameName);
+char* findValue(GameData* data, char* keyName);
+GameData* createGame(SaveFile* file, char* gameName);
+void putValue(GameData* data, char* key, char* val);
 
 
 
--- safefiles/save.h
+++ safefiles/save.h
@@ -1,3 +1,26 @@
 #ifndef SAVE_H
 #define SAVE_H
+struct Property{
+    char* key;
+    char* value;
+};
+struct GameData{
+    char* gameName;
+    Property* properties;
+    int num_properties;
+};
+struct SaveFile{
+    GameData* games;
+    int num_games;
+};
+
+SaveFile* loadSaveFile();
+void updateSaveFile();
+void freeSaveFile(SaveFile*);
+
+GameData* findGame(SaveFile* file, char* gameName);
+char* findValue(GameData* data, char* keyName);
+GameData* createGame(SaveFile* file, char* gameName);
+void putValue(GameData* data, char* key, char* val);
+
 #endif
