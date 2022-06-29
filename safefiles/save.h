3817a5e5215ffe5222b71c77ed41c84cb770e95b
Frobeniusnorm@Gwendolyn <david.schwarzbeck@web.de>
2022-06-29 13:11:13 +0200

    resolved struct namespace error (weird that it didn't happen in NetBeans)

--- safefiles/save.h
+++ safefiles/save.h
@@ -1,26 +1,26 @@
 #ifndef SAVE_H
 #define SAVE_H
-struct Property{
-    char* key;
-    char* value;
-};
-struct GameData{
-    char* gameName;
-    Property* properties;
-    int num_properties;
-};
-struct SaveFile{
-    GameData* games;
-    int num_games;
-};
+typedef struct Property {
+  char *key;
+  char *value;
+} Property;
+typedef struct GameData {
+  char *gameName;
+  Property *properties;
+  int num_properties;
+} GameData;
+typedef struct SaveFile {
+  GameData *games;
+  int num_games;
+} SaveFile;
 
-SaveFile* loadSaveFile();
+SaveFile *loadSaveFile();
 void updateSaveFile();
-void freeSaveFile(SaveFile*);
+void freeSaveFile(SaveFile *);
 
-GameData* findGame(SaveFile* file, char* gameName);
-char* findValue(GameData* data, char* keyName);
-GameData* createGame(SaveFile* file, char* gameName);
-void putValue(GameData* data, char* key, char* val);
+GameData *findGame(SaveFile *file, char *gameName);
+char *findValue(GameData *data, char *keyName);
+GameData *createGame(SaveFile *file, char *gameName);
+void putValue(GameData *data, char *key, char *val);
 
 #endif
